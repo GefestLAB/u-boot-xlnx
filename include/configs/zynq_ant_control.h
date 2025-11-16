1667e85f9a5975a1927361981894a33f0e79b15b
Artem Anogda <anogdaa@gmail.com>
2025-11-16 12:57:09 +0200

    add board

---
+++ include/configs/zynq_ant_control.h
@@ -0,0 +1,20 @@
+
+#ifndef __CONFIG_GLAB_ANT_CONTROL_H
+#define __CONFIG_GLAB_ANT_CONTROL_H
+
+#define CFG_SYS_SDRAM_BASE	0x00000000
+#define CFG_SYS_SDRAM_SIZE	0x40000000
+
+#define CFG_EXTRA_ENV_SETTINGS \
+	"pxefile_addr_r=0x2000000\0" \
+	"scriptaddr=0x3000000\0" \
+	"kernel_addr_r=0x2000000\0" \
+	"fdt_high=0xefff000\0" \
+	"initrd_high=0xefff000\0" \
+	"devnum=0\0" \
+	"wdstop=mw f8005000 ABC000\0" \
+	BOOTENV
+
+#include <configs/zynq-common.h>
+
+#endif /* __CONFIG_GLAB_ANT_CONTROL_H */
